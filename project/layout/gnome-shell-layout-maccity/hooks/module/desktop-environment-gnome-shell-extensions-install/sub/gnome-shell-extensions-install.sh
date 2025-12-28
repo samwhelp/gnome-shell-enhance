7813438f4912d12665c3b9b05d806d522624f5be
samwhelp <samwhelp@users.noreply.github.com>
2025-12-28 09:02:02 +0800

    fix

--- project/layout/gnome-shell-layout-maccity/hooks/module/desktop-environment-gnome-shell-extensions-install/sub/gnome-shell-extensions-install.sh
+++ project/layout/gnome-shell-layout-maccity/hooks/module/desktop-environment-gnome-shell-extensions-install/sub/gnome-shell-extensions-install.sh
@@ -10,5 +10,14 @@ sub_gnome_shell_extensions_all_install_via_gext () {
 
 	echo sub_gnome_shell_extensions_all_install_via_gext
 
+	sys_gnome_shell_extensions_each_install_via_gext "blur-my-shell@aunetx"
+
+}
+
+sys_gnome_shell_extensions_each_install_via_gext () {
+
+	local the_extension_id="${1}"
+
+	gext -F install "${the_extension_id}"
 
 }
